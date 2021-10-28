48f87c944a119102432a5f14a988bfc7accd9731
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-10-28 02:33:20 +0100

    Added ripper to watch processes

---
+++ src/process/ripper.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ripper.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 22:22:15 by mgueifao          #+#    #+#             */
+/*   Updated: 2021/10/28 02:32:38 by mgueifao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+void	*ripper(void *sym)
+{
+	t_sym	*s;
+	int		i;
+
+	s = sym;
+	sem_wait(s->sem_died);
+	sem_wait(s->sem_died);
+	if (s->tr_me)
+	{
+		return (NULL);
+	}
+	s->tr_me = 1;
+	i = 0;
+	while (i < s->pcount)
+	{
+		sem_post(s->sem_done);
+		kill(s->pids[i], SIGTERM);
+		i++;
+	}
+	return (NULL);
+}
+
+void	*completionist(void *sym)
+{
+	t_sym	*s;
+	int		i;
+
+	s = sym;
+	i = -1;
+	sem_wait(s->sem_done);
+	while (i++ < s->pcount)
+	{
+		sem_wait(s->sem_done);
+	}
+	if (s->tr_me)
+	{
+		return (NULL);
+	}
+	s->tr_me = 1;
+	i = 0;
+	while (i < s->pcount)
+	{
+		kill(s->pids[i], SIGTERM);
+		i++;
+	}
+	sem_post(s->sem_died);
+	return (NULL);
+}
